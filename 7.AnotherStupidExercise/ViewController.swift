dadd32e6d0df84cb98ce1d3749e42074eb585a53
Chi Shing <chishingt@outlook.com>
2017-05-13 18:02:51 +0800

    Completed project

--- 7.AnotherStupidExercise/ViewController.swift
+++ 7.AnotherStupidExercise/ViewController.swift
@@ -10,16 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var multipleField: UITextField!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var newMultiple: UILabel!
+    @IBOutlet weak var OriginalMultiple: UILabel!
+    @IBOutlet weak var OldMultiple: UILabel!
+    @IBOutlet weak var Sign: UILabel!
+    
+    var multiple: Int = 0
+    var temporarysum: Int = 0
+    
+    @IBAction func playGame(_ sender:UIButton) {
+    if multipleField.text != "" && Int(multipleField.text!)! >= 1 {
+            hide()
+            multiple = Int(multipleField.text!)!
+            OriginalMultiple.text! = String(multiple)
+            newMultiple.text! = "0"
+            OldMultiple.text! = "0"
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addMultiple(_ sender:UIButton) {
+        OldMultiple.text! = showText(numb:multiple)
+        multiple += Int(OriginalMultiple.text!)!
+        newMultiple.text! = showText(numb:multiple)
     }
 
-
+    @IBAction func reset (_ sender:UIButton){
+        unhide()
+    }
+    
+    func showText(numb:Int) -> String{
+        return "\(numb)"
+    }
+    
+    func hide(){
+        addButton.isHidden = false
+        playButton.isHidden = true
+        multipleField.isHidden = true
+        newMultiple.isHidden = false
+        OldMultiple.isHidden = false
+        OriginalMultiple.isHidden = false
+        resetButton.isHidden = false
+        Sign.isHidden = false
+    }
+    func unhide(){
+        addButton.isHidden = true
+        playButton.isHidden = false
+        multipleField.isHidden = false
+        newMultiple.isHidden = true
+        OldMultiple.isHidden = true
+        OriginalMultiple.isHidden = true
+        resetButton.isHidden = true
+        Sign.isHidden = true
+    }
 }
 
